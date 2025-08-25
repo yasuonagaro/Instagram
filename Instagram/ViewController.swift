33a91ce062fb5df2a6668b02a1be10b2b7a8d42c
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-25 11:32:24 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by ynagaro on 2025/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramTests/InstagramTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTests.swift
+//  InstagramTests
+//
+//  Created by ynagaro on 2025/08/25.
+//
+
+import Testing
+@testable import Instagram
+
+struct InstagramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
